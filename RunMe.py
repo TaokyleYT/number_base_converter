a4d605f2cc2696761945c4951f9692f0f45230ec
TaokyleYT <102730210+TaokyleYT@users.noreply.github.com>
2022-09-25 10:30:35 +0800

    Update RunMe.py

--- RunMe.py
+++ RunMe.py
@@ -1,3 +1,6 @@
+#This program is initializing the database only.
+#If I am being run before and the database's file isn't being deleted, I needn't run again or error may come up.
+#to check if the database file is still on your local, you can run the converter's code. If it says TABLE not exist, run me now.
 #database init program
 import sqlite3
 con=sqlite3.connect("NumberBaseConverterLoggingGithub.db")
