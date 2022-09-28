be43b7f37babc6055a29b370289d7959211630e6
TaokyleYT <102730210+TaokyleYT@users.noreply.github.com>
2022-09-28 10:56:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import runpy
 from os import system as sys, name as user
-#python file name need to be changed or added for your favour as below if anything is changed
-file=['Base10.py', 'Base36.py']
+#please change the corrsponding file name if you changed them, or else error may occor
+file = ['Base10.py', 'Base36.py']
 while True:
-  mode=input('Enter 0 for max base 10(result float) method, enter 1 for max base 36(result string) method\nPlease enter method you want to use: ')
-  try:
-    int(mode)
-  except:
-    print('\nPlease enter a valid number\n')
-  else:
-    mode=int(mode)
-    if mode > 1 or mode < 0:
-      print('\nPlease enter a valid number\n')
+    mode = input(
+        'Enter 0 for max base 10(result float) method, enter 1 for max base 36(result string) method\nPlease enter method you want to use: '
+    )
+    try:
+        int(mode)
+    except:
+        print('\nPlease enter a valid number\n')
     else:
-      break
+        mode = int(mode)
+        if mode > 1 or mode < 0:
+            print('\nPlease enter a valid number\n')
+        else:
+            break
 if user == 'nt':
-  _=sys('cls')
+    _ = sys('cls')
 else:
-  _=sys('clear')
+    _ = sys('clear')
 runpy.run_path(path_name=file[mode])
